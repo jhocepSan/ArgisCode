5971deef6d4c84b253baf5b4f13d03f83b83bbce
Juan Jose <sajhyjhocep@gmail.com>
2022-02-17 18:24:01 -0400

    codigo mejorado funcionando busqueda lineal

--- nuevaLinea/busquedaPuntoParalelo.py
+++ nuevaLinea/busquedaPuntoParalelo.py
@@ -1,16 +1,17 @@
 import arcpy,math
+import pythonaddins
 
 class BusquedaPuntoParalelo(object):
     def __init__(self):
-        self.fileArboles="1065_san_jorge_arboles_merge"
+        self.fileArboles="31_arboles"
         self.fileAuxi="auxiliar"
-        self.fileCamino="1065_san_jorge_caminos"
-        self.fileNoCultivable="1065_san_jorge_area_nocultivable"
-        self.fileDesechos="1065_san_jorge_desechos_depurados"
+        self.fileCamino="31_caminos"
+        self.fileNoCultivable="31_no_cultivable"
+        self.fileDesechos="31_desechos"
         self.nrLinea=0
         self.clearProg()
     def clearProg(self):
-        self.distancia=0
+        self.distancia=10000
         self.punto1=[]
         self.punto2=[]
         self.angulo=0
@@ -27,14 +28,17 @@ class BusquedaPuntoParalelo(object):
     def getPendiente(self,punto1,punto2):
         m= (punto2[1]-punto1[1])/(punto2[0]-punto1[0])
         return m
-    def esValidoPunto(self,fid):
-        valor=True
-        with arcpy.da.SearchCursor(self.fileArboles,["SHAPE@XY","nro_linea"],""""FID"={}""".format(fid)) as cursor:
-            for row in cursor:
-                if row[1]==-1:
-                    print("punto no valido: "+str(fid))
-                    valor=False 
-        return valor
+    def completarCampos(self):
+        nro_linea=arcpy.ListFields(self.fileArboles,"nro_linea")
+        valido=arcpy.ListFields(self.fileArboles,"valido")
+        if not nro_linea:
+            arcpy.AddField_management(self.fileArboles,"nro_linea","LONG")
+        if not valido:
+            arcpy.AddField_management(self.fileArboles,"valido","LONG")
+        if not valido or not nro_linea:
+            pythonaddins.MessageBox ("Creado el campo necesario", "Correcto")
+        else:
+            pythonaddins.MessageBox ("Ya tiene los campos necesarios", "Informacion")
     def getSeleccion(self):
         listaPuntos=list()
         dato=arcpy.SelectLayerByLocation_management(self.fileArboles,"WITHIN_A_DISTANCE",self.fileAuxi,"100 Centimeters","NEW_SELECTION")
@@ -42,10 +46,10 @@ class BusquedaPuntoParalelo(object):
         print("total Seleccionados: {}".format(arcpy.GetCount_management(self.fileArboles)[0]))
         for i in range(nroPuntos):
             fid=dato.getOutput(0).getSelectionSet()[i]
-            if self.esValidoPunto(fid):
-                with arcpy.da.SearchCursor(self.fileArboles,["SHAPE@XY","nro_linea"],""""FID"={}""".format(fid)) as cursor:
-                    for row in cursor:
-                        listaPuntos.append((fid,row[0],row[1]))
+            with arcpy.da.SearchCursor(self.fileArboles,["SHAPE@XY","nro_linea","valido"],""""FID"={}""".format(fid)) as cursor:
+                for row in cursor:
+                    if row[1]!=-1 and row[2]!=1:
+                        listaPuntos.append((fid,row[0],row[1],row[2]))
         print("Los Puntos Son: {}".format(listaPuntos))
         return listaPuntos,len(listaPuntos)
     def dentroDeArea(self,capa1,capa2):
@@ -67,3 +71,62 @@ class BusquedaPuntoParalelo(object):
         for i in datos:
             d=d or i
         return d
+    def setNroLine(self,fid,nro_linea,visitado):
+        with arcpy.da.UpdateCursor(self.fileArboles, ["nro_linea","valido"],""""FID"={}""".format(fid)) as curs:
+            for row in curs:
+                row[0] = nro_linea
+                row[1] = visitado
+                curs.updateRow(row)
+    def getPuntoSugerido(self,distancia,angulo):
+        xa=self.punto2[1][0]+distancia*math.cos(angulo)
+        ya=self.punto2[1][1]+distancia*math.sin(angulo)
+        return(xa,ya)
+    def limpiarAux(self):
+        arcpy.DeleteFeatures_management(self.fileAuxi)
+    def colocarPunto(self,puntoNew,puntoAntiguo):
+        arcpy.DeleteFeatures_management(self.fileAuxi)
+        with arcpy.da.InsertCursor(self.fileAuxi,"SHAPE@XY") as cursor:
+            cursor.insertRow([puntoNew])
+        with arcpy.da.InsertCursor(self.fileAuxi,"SHAPE@XY") as cursor:
+            cursor.insertRow([puntoAntiguo])
+        del cursor
+        arcpy.RefreshActiveView()
+    def busquedaLineal(self):
+        #formato puntos [fid,punto,nro_linea,valido]
+        distancia=0
+        angulo=0
+        while self.runProg:
+            puntos,np=self.getSeleccion()
+            if np==2:
+                distancia=self.getDistancia(puntos[0][1],puntos[1][1])
+                if distancia<self.distancia:
+                    self.distancia=distancia
+                else:
+                    distancia=self.distancia
+                if puntos[0][3]==2:
+                    print(puntos[0][1])
+                    angulo=self.getAngulo(puntos[0][1],puntos[1][1])
+                    self.punto1=puntos[0]
+                    self.punto2=puntos[1]
+                else:
+                    angulo=self.getAngulo(puntos[1][1],puntos[0][1])
+                    self.punto1=puntos[1]
+                    self.punto2=puntos[0]
+                print("angulo: {} , distancia: {}".format(angulo,distancia))
+                print("......modificamos valido, nr_linea........")
+                self.setNroLine(self.punto1[0],self.nrLinea,1)
+                self.setNroLine(self.punto2[0],0,2)
+                self.valido=True
+            elif np==1:
+                distancia+=0.2
+                self.valido=True
+            elif np>=3:
+                self.runProg=False
+            if self.valido:
+                newP=self.getPuntoSugerido(distancia,angulo)
+                self.colocarPunto(self.punto2[1],newP)
+                self.valido=False
+                if self.estaEnArea==True:
+                    self.setNroLine(self.punto2[0],self.nrLinea,1)
+                    self.runProg=False
+                    self.limpiarAux()
